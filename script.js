c9bcde3b1661a9d37085e5fe2c02f39e853f3e37
Marcio-Brust <brustmw21@gmail.com>
2022-06-05 15:52:45 -0300

    subindo arquivos do projeto

---
+++ script.js
@@ -0,0 +1,10 @@
+/* const body = document.querySelector(".body2");
+const width = screen.width;
+
+function tamanhoDoCorpo() {
+  body.innerWidth = width;
+  body.style.width = `${width}px`;
+  console.log(body.innerWidth);
+}
+body.addEventListener("load", tamanhoDoCorpo);
+tamanhoDoCorpo(); */
